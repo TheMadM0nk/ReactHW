04eb3d746843a4670091e52a701fdc3590c4798b
K.Denis <k.denis.dev@hotmail.com>
2022-01-16 14:03:53 +0200

    1st home work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import style from './styles.module.css';
+
+
+class Menu extends React.Component {
+
+  render() {
+    return (
+      <div className={style.menu}>
+        <div className={`${style.menu_box} ${style.container}`} >
+          <h2>My React App</h2>
+        </div>
+      </div>
+    )
+  }
+};
+
+const text = 'Hello everybody!';
+
+function Message(prop) {
+
+  return (
+    <div className={style.msg_container}>
+      <h3>{prop}</h3>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <Menu />
+
+    <main className={style.main_center}>
+      {Message(text)}
+    </main>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
