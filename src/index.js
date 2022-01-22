924c0a4dc7dbd9fc57e12850ef6c38b214adec4b
K.Denis <k.denis.dev@hotmail.com>
2022-01-22 11:49:39 +0200

    Added fuctionality and styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import style from './styles.module.css';
@@ -17,25 +17,97 @@ class Menu extends React.Component {
   }
 };
 
-const text = 'Hello everybody!';
 
-const Message = (prop) => {
+
+const App = () => {
+
+  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const myRef = React.createRef();
+
+  useEffect(() => {
+    myRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    const lastMessage = messageList[messageList.length - 1];
+    let timerId = null;
+
+    if (messageList.length && lastMessage?.author !== "Bot:") {
+      timerId = setTimeout(() => {
+        setMessageList([
+          ...messageList,
+          { author: "Bot:", message: "Message recived!" },
+        ]);
+      }, 500);
+    }
+
+    return () => clearInterval(timerId);
+  }, [messageList]);
+
+  const Clock = () => {
+    const [time, setTime] = useState(new Date());
+
+    const refreshClock = () => {
+      setTime(new Date());
+    }
+    useEffect(() => {
+      const timer = setInterval(refreshClock, 1000);
+      return () => clearInterval(timer);
+    }, [time]);
+
+    return (
+      <span className={style.clock}>
+        Current is {time.toLocaleTimeString('Ru-ru')}
+      </span>
+    );
+  }
+
+  const sendMsg = () => {
+    if (message) {
+      setMessageList([...messageList, { author: 'User:', message: myRef.current.value }]);
+      setMessage('');
+    } else {
+      alert("Введите сообщение");
+    };
+
+  }
+
+  const handlePressInput = (event) => {
+    if (event.code === "Enter") {
+      sendMsg();
+    }
+  };
 
   return (
-    <div className={style.msg_container}>
-      <h3>{prop}</h3>
-    </div>
+
+    <main className={style.main_center}>
+      <div className={style.msgField}>
+        {messageList.map((msg, index) => (
+          <div className={style.msg_container} key={index}>
+            <h4>{msg.author}</h4>
+            <p>{msg.message}</p>
+          </div>))}
+      </div>
+
+      <div className={style.inputForm}>
+        <input ref={myRef} value={message} onKeyPress={handlePressInput} onChange={(event) => setMessage(event.target.value)} className={style.textarea} />
+        <div className={style.inputForm_btnBox}>
+          <Clock />
+          <button className={style.btn} onClick={sendMsg}>Send</button>
+        </div>
+      </div>
+    </main>
   );
-};
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
 
     <Menu />
-
-    <main className={style.main_center}>
-      {Message(text)}
-    </main>
+    <App />
 
   </React.StrictMode>,
   document.getElementById('root')
